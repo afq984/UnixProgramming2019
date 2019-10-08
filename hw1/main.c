3ea65e643b471c6eace4e7445327ff558446d56c
YU LI YU <afg984@gmail.com>
2019-10-08 10:51:52 +0800

    print hex address and decimal port

---
+++ hw1/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <errno.h>
+
+const char row_format[] = "%-5s %-23s %-23s %s\n";
+const char _column0[] = "Proto";
+const char _column1[] = "Local Address";
+const char _column2[] = "Foreign Address";
+const char _column3[] = "PID/Program name and arguments";
+
+struct Connection {
+    int inode;
+    struct Process* proc;
+};
+
+struct Process {
+    int pid;
+    char* exe;
+    char* cmdline;
+};
+
+void fatal(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    exit(2);
+}
+
+// skips 1 line
+// returns 1 on unexpected EOF
+int skipline(FILE* file) {
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void format_address(char out[45], const char* addr, int port) {
+    snprintf(out, 45, "%s:%d", addr, port);
+}
+
+void process_family(const char* family) {
+    char filename[] = "/proc/net/tcp6";
+    strncpy(filename+10, family, 5);
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        fatal("error opening %s", filename);
+    }
+    if (skipline(file)) {
+        fatal("unexpected EOF processing %s", filename);
+    }
+    char local_addr[33];
+    int local_port;
+    char remote_addr[33];
+    int remote_port;
+    int inode;
+    while (fscanf(file, "%*s %[0-9A-Fa-f]:%x %[0-9A-Fa-f]:%x %*s %*s %*s %*s %*d %*d %d", local_addr, &local_port, remote_addr, &remote_port, &inode) != EOF) {
+        if (skipline(file)) {
+            fatal("unexpected EOF processing %s", filename);
+        }
+        char fla[45];
+        char fra[45];
+        format_address(fla, local_addr, local_port);
+        format_address(fra, remote_addr, remote_port);
+        printf(row_format, family, fla, fra, "?/?");
+    }
+    fclose(file);
+}
+
+int main(int argc, char** argv) {
+    process_family("tcp");
+    process_family("tcp6");
+    process_family("udp");
+    process_family("udp6");
+}
